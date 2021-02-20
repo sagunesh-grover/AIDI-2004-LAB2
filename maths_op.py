d9510e31741c5a1bfe10855e552a3e5d2d9bd536
sagunesh.grover <saguneshgrover@gmail.com>
2021-02-20 09:35:43 +0530

    gcp basic python program added

---
+++ maths_op.py
@@ -0,0 +1,17 @@
+#simple maths program through gcp
+def add(a,b):
+    return a+b
+def sub(a,b):
+    return a-b
+def mul(a,b):
+    return a*b
+def div(a,b):
+    return a/b
+
+a = 10
+b = 2
+
+print("Addition = "+str(add(10,2)))
+print("Subtraction = "+str(sub(10,2)))
+print("Multiplication= "+str(mul(10,2)))
+print("Division= "+str(div(10,2)))
